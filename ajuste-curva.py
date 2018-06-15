a7fa0de914c07a359bd1b9b6d44f256f44792079
renannascimento <renanmateusbn@gmail.com>
2018-06-14 23:54:35 -0300

    criacao do dataset

--- ajuste-curva.py
+++ ajuste-curva.py
@@ -1,20 +1,38 @@
 import anfis.anfis as c_anfis
 import anfis.membership as amf
-import numpy
+import numpy as np
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+def curve(x, y):
+    return math.sin(x)*math.sin(y)/x*y
+
+size = 1000
+x = [np.random.uniform(-10, 10) for i in range(0, size)]
+y = [np.random.uniform(-10, 10) for i in range(0, size)]
+z = [curve(x[i], y[i]) for i in range(0, size)]
 
 
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
-ts = numpy.loadtxt("trainingSet.txt", usecols=[1,2,3])#numpy.loadtxt('c:\\Python_fiddling\\myProject\\MF\\trainingSet.txt',usecols=[1,2,3])
-X = ts[:,0:2]
-Y = ts[:,2]
+ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+
+plt.show()
 
 
-mf = [[['gaussmf',{'mean':0.,'sigma':1.}],['gaussmf',{'mean':-1.,'sigma':2.}],['gaussmf',{'mean':-4.,'sigma':10.}],['gaussmf',{'mean':-7.,'sigma':7.}]],
-            [['gaussmf',{'mean':1.,'sigma':2.}],['gaussmf',{'mean':2.,'sigma':3.}],['gaussmf',{'mean':-2.,'sigma':10.}],['gaussmf',{'mean':-10.5,'sigma':5.}]]]
+X = np.array([[x[i], y[i]] for i in range(0, size)])
+Y = np.array(z)
+
+
+mf = [[['gaussmf',{'mean':-10.,'sigma':1.}],['gaussmf',{'mean':-5.,'sigma':1.}],['gaussmf',{'mean':5.,'sigma':1.}],['gaussmf',{'mean':10.,'sigma':1.}]],
+            [['gaussmf',{'mean':-10.,'sigma':1.}],['gaussmf',{'mean':-5.,'sigma':1.}],['gaussmf',{'mean':5.,'sigma':1.}],['gaussmf',{'mean':10,'sigma':1.}]]]
 
 mfc = amf.membershipfunction.MemFuncs(mf)
 anf = c_anfis.ANFIS(X, Y, mfc)
-anf.trainHybridJangOffLine(epochs=5)
+anf.trainHybridJangOffLine(epochs=10)
 print round(anf.consequents[-1][0],6)
 print round(anf.consequents[-2][0],6)
 print round(anf.fittedValues[9][0],6)
