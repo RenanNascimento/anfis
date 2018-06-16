07a692697ad5201585a4625c02db803ab0e96d64
Nícolas Maduro <nicolasamaduro@gmail.com>
2018-06-15 21:17:02 -0300

    mudança da função e do parametros

--- ajuste-curva.py
+++ ajuste-curva.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def curve(x, y):
-    return math.sin(x)*math.sin(y)/x*y
+    return (math.sin(x)*math.sin(y))/(x*y)
 
-size = 1000
+size = 10000
 x = [np.random.uniform(-10, 10) for i in range(0, size)]
 y = [np.random.uniform(-10, 10) for i in range(0, size)]
 z = [curve(x[i], y[i]) for i in range(0, size)]
-
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -22,23 +21,18 @@ ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
 
 plt.show()
 
-
 X = np.array([[x[i], y[i]] for i in range(0, size)])
 Y = np.array(z)
 
-
-mf = [[['gaussmf',{'mean':-10.,'sigma':1.}],['gaussmf',{'mean':-5.,'sigma':1.}],['gaussmf',{'mean':5.,'sigma':1.}],['gaussmf',{'mean':10.,'sigma':1.}]],
-            [['gaussmf',{'mean':-10.,'sigma':1.}],['gaussmf',{'mean':-5.,'sigma':1.}],['gaussmf',{'mean':5.,'sigma':1.}],['gaussmf',{'mean':10,'sigma':1.}]]]
+mf = [[['gaussmf',{'mean':-8.,'sigma':1}],['gaussmf',{'mean':-3,'sigma':1}],['gaussmf',{'mean':3,'sigma':1}],['gaussmf',{'mean':8,'sigma':1}]],
+            [['gaussmf',{'mean':-10,'sigma':1}],['gaussmf',{'mean':-5,'sigma':1}],['gaussmf',{'mean':5,'sigma':1}],['gaussmf',{'mean':10,'sigma':1}]]]
 
 mfc = amf.membershipfunction.MemFuncs(mf)
 anf = c_anfis.ANFIS(X, Y, mfc)
-anf.trainHybridJangOffLine(epochs=10)
+anf.trainHybridJangOffLine(epochs=50)
 print round(anf.consequents[-1][0],6)
 print round(anf.consequents[-2][0],6)
 print round(anf.fittedValues[9][0],6)
-
-if round(anf.consequents[-1][0],6) == -5.275538 and round(anf.consequents[-2][0],6) == -1.990703 and round(anf.fittedValues[9][0],6) == 0.002249:
-	print 'test is good'
 
 anf.plotErrors()
 anf.plotResults()
